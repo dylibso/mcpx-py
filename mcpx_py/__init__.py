54f10da7229c0466d01b854b83368d5a665c5e69
zach <zach@dylibso.com>
2025-03-05 22:13:40 -0800

    cleanup: export more

--- mcpx_py/__init__.py
+++ mcpx_py/__init__.py
@@ -1,8 +1,11 @@
 from .chat import ChatConfig, Chat
-import mcp_run as client
+from mcpx_pydantic_ai import Agent, BaseModel
+import mcp_run
 
 __all__ = [
     "Chat",
     "ChatConfig",
-    "client",
+    "Agent",
+    "BaseModel",
+    "mcp_run",
 ]
